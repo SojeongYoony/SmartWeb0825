2a03ad2b046994fbaa013772cae35b180de4c089
SoJeongYoon <89592076+SojeongYoony@users.noreply.github.com>
2021-11-06 22:24:48 +0900

    Add files via upload

---
+++ jspstudy/workspace/05_MVC/src/model/Today.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Today {
+
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// 여기야.. JSP -> Servlet(MyController : 신호등) -> 최종 java File까지 도착 
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 E요일");
+		String result = sdf.format(new Date());
+		
+		request.setAttribute("result", result);
+		
+	}
+
+	
+}

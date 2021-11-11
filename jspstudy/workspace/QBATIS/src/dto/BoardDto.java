7365dcd2b54ced0872e38bc6da44194e33e98384
SoJeongYoon <89592076+SojeongYoony@users.noreply.github.com>
2021-11-11 21:52:29 +0900

    Add files via upload

---
+++ jspstudy/workspace/QBATIS/src/dto/BoardDto.java
@@ -0,0 +1,45 @@
+package dto;
+
+public class BoardDto {
+	
+	private Long num;
+	private String title;
+	private String writer;
+	private String content;
+	private String register;
+	
+	
+	public Long getNum() {
+		return num;
+	}
+	public void setNum(Long num) {
+		this.num = num;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getRegister() {
+		return register;
+	}
+	public void setRegister(String register) {
+		this.register = register;
+	}
+	
+	
+
+}
